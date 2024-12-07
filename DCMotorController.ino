97da9f38ea05bb03374105ab1417e1a0c3c470bc
Ricardo Sabino <sabino_lrs@hotmail.com>
2024-12-07 13:09:19 -0300

    Update DCMotorController.ino

--- DCMotorController.ino
+++ DCMotorController.ino
@@ -1,167 +1,126 @@
-#define RIGHT_FRONT_ENGINE_IN1 52
-#define RIGHT_FRONT_ENGINE_IN2 50
-#define FRONT_LEFT_ENGINE_IN3 48
-#define FRONT_LEFT_ENGINE_IN4 46
-
-#define RIGHT_REAR_ENGINE_IN1 31
-#define RIGHT_REAR_ENGINE_IN2 33
-#define LEFT_REAR_ENGINE_IN3 35
-#define LEFT_REAR_ENGINE_IN4 37
-
-// Orientation
-#define FORWARD "f"
-#define BACK "b"
-#define LEFT "l"
-#define RIGHT "r"
-#define STOP "s"
-
-// Sets the output pins
-void setPinModeOut(int engine)
-{
-    pinMode(engine, OUTPUT);
-}
-
-// Active at high logic level, the pins responsible for DC motor rotation
-void active5V(int engine)
-{
-    digitalWrite(engine, HIGH);
-}
-
-// Active at low logic level, the pins responsible for the rotation of the DC motor
-void activeGND(int engine)
-{
-    digitalWrite(engine, LOW);
-}
-
-// Sets the delay time between received commands
-void runtime(int timeDelay)
-{
-    delay(timeDelay);
-    stopTheRobot();
-}
-
-void setup()
-{
-    Serial.begin(9600);
-
-    setPinModeOut(RIGHT_FRONT_ENGINE_IN1);
-    setPinModeOut(RIGHT_FRONT_ENGINE_IN2);
-    setPinModeOut(FRONT_LEFT_ENGINE_IN3);
-    setPinModeOut(FRONT_LEFT_ENGINE_IN4);
-
-    setPinModeOut(RIGHT_REAR_ENGINE_IN1);
-    setPinModeOut(RIGHT_REAR_ENGINE_IN2);
-    setPinModeOut(LEFT_REAR_ENGINE_IN3);
-    setPinModeOut(LEFT_REAR_ENGINE_IN4);
-}
-
-// Engine movement clockwise
-void engineMovementClockwise(int pinIn1, int pinIn2)
-{
-    active5V(pinIn1);
-    activeGND(pinIn2);
-}
-
-// Engine movement counterclockwise
-void engineMovementCounterclockwise(int pinIn1, int pinIn2)
-{
-    activeGND(pinIn1);
-    active5V(pinIn2);
-}
-
-// Dead-spot
-void deadSpot(int pinIn1, int pinIn2)
-{
-    activeGND(pinIn1);
-    activeGND(pinIn2);
-}
-
-// Braked engine
-void braked(int pinIn1, int pinIn2)
-{
-    active5V(pinIn1);
-    active5V(pinIn2);
-}
-
-// Move the robot forward
-void moveTheRobotForward()
-{
-    engineMovementClockwise(RIGHT_FRONT_ENGINE_IN1, RIGHT_FRONT_ENGINE_IN2);
-    engineMovementCounterclockwise(FRONT_LEFT_ENGINE_IN3, FRONT_LEFT_ENGINE_IN4);
-    engineMovementClockwise(LEFT_REAR_ENGINE_IN3, LEFT_REAR_ENGINE_IN4);
-    engineMovementCounterclockwise(RIGHT_REAR_ENGINE_IN1, RIGHT_REAR_ENGINE_IN2);
-}
-
-// Move the robot back
-void moveTheRobotBack()
-{
-    engineMovementCounterclockwise(RIGHT_FRONT_ENGINE_IN1, RIGHT_FRONT_ENGINE_IN2);
-    engineMovementClockwise(FRONT_LEFT_ENGINE_IN3, FRONT_LEFT_ENGINE_IN4);
-    engineMovementCounterclockwise(LEFT_REAR_ENGINE_IN3, LEFT_REAR_ENGINE_IN4);
-    engineMovementClockwise(RIGHT_REAR_ENGINE_IN1, RIGHT_REAR_ENGINE_IN2);
-}
-
-// Stop the robot
-void stopTheRobot()
-{
-    braked(RIGHT_FRONT_ENGINE_IN1, RIGHT_FRONT_ENGINE_IN2);
-    braked(FRONT_LEFT_ENGINE_IN3, FRONT_LEFT_ENGINE_IN4);
-    braked(RIGHT_REAR_ENGINE_IN1, RIGHT_REAR_ENGINE_IN2);
-    braked(LEFT_REAR_ENGINE_IN3, LEFT_REAR_ENGINE_IN4);
-}
-
-// Rotate the robot right
-void rotateTheRobotRight()
-{
-    engineMovementCounterclockwise(FRONT_LEFT_ENGINE_IN3, FRONT_LEFT_ENGINE_IN4);
-    engineMovementCounterclockwise(RIGHT_REAR_ENGINE_IN1, RIGHT_REAR_ENGINE_IN2);
-    braked(RIGHT_FRONT_ENGINE_IN1, RIGHT_FRONT_ENGINE_IN2);
-    braked(LEFT_REAR_ENGINE_IN3, LEFT_REAR_ENGINE_IN4);
-}
-
-// Rotate the robot left
-void rotateTheRobotLeft()
-{
-    engineMovementClockwise(RIGHT_FRONT_ENGINE_IN1, RIGHT_FRONT_ENGINE_IN2);
-    engineMovementClockwise(LEFT_REAR_ENGINE_IN3, LEFT_REAR_ENGINE_IN4);
-    braked(FRONT_LEFT_ENGINE_IN3, FRONT_LEFT_ENGINE_IN4);
-    braked(RIGHT_REAR_ENGINE_IN1, RIGHT_REAR_ENGINE_IN2);
-}
-
-void loop()
-{
-
-    auto timeDelay = 2000;
-    String commandRead = "";
-
-    if (Serial.available())
-    {
-        while (Serial.available())
-        {
-            commandRead += (char)Serial.read();
-        }
-    }
-
-    switch (commandRead)
-    {
-    case FORWARD:
-        moveTheRobotForward();
-        runtime(timeDelay);
-        break;
-    case BACK:
-        moveTheRobotBack();
-        runtime(timeDelay);
-        break;
-    case LEFT:
-        rotateTheRobotLeft();
-        runtime(timeDelay);
-        break;
-    case RIGHT:
-        rotateTheRobotRight();
-        runtime(timeDelay);
-        break;
-    case STOP:
-        stopTheRobot();
-        break;
-    }
-}
+#define ENGINE_COUNT 8  
+  
+const int enginePins[ENGINE_COUNT] = {  
+  52, 50, 48, 46, 31, 33, 35, 37  
+};  
+  
+enum Direction {  
+  FORWARD = 'f',  
+  BACK = 'b',  
+  LEFT = 'l',  
+  RIGHT = 'r',  
+  STOP = 's'  
+};  
+  
+// Set all engine pins to output mode  
+void setPinModeOut() {  
+  for (int i = 0; i < ENGINE_COUNT; i++) {  
+    pinMode(enginePins[i], OUTPUT);  
+  }  
+}  
+  
+// Activate pin with 5V logic level  
+void active5V(int engine) {  
+  digitalWrite(engine, HIGH);  
+}  
+  
+// Activate pin with GND logic level  
+void activeGND(int engine) {  
+  digitalWrite(engine, LOW);  
+}  
+  
+// Move engine clockwise  
+void engineMovementClockwise(int pinIn1, int pinIn2) {  
+  active5V(pinIn1);  
+  activeGND(pinIn2);  
+}  
+  
+// Move engine counterclockwise  
+void engineMovementCounterclockwise(int pinIn1, int pinIn2) {  
+  activeGND(pinIn1);  
+  active5V(pinIn2);  
+}  
+  
+// Brake the engine  
+void braked(int pinIn1, int pinIn2) {  
+  active5V(pinIn1);  
+  active5V(pinIn2);  
+}  
+  
+// Move the robot forward  
+void moveTheRobotForward() {  
+  engineMovementClockwise(enginePins[0], enginePins[1]);  
+  engineMovementCounterclockwise(enginePins[2], enginePins[3]);  
+  engineMovementClockwise(enginePins[4], enginePins[5]);  
+  engineMovementCounterclockwise(enginePins[6], enginePins[7]);  
+}  
+  
+// Move the robot backward  
+void moveTheRobotBack() {  
+  engineMovementCounterclockwise(enginePins[0], enginePins[1]);  
+  engineMovementClockwise(enginePins[2], enginePins[3]);  
+  engineMovementCounterclockwise(enginePins[4], enginePins[5]);  
+  engineMovementClockwise(enginePins[6], enginePins[7]);  
+}  
+  
+// Stop all engines  
+void stopTheRobot() {  
+  for (int i = 0; i < ENGINE_COUNT; i += 2) {  
+    braked(enginePins[i], enginePins[i + 1]);  
+  }  
+}  
+  
+// Rotate the robot to the right  
+void rotateTheRobotRight() {  
+  engineMovementCounterclockwise(enginePins[2], enginePins[3]);  
+  engineMovementCounterclockwise(enginePins[6], enginePins[7]);  
+  braked(enginePins[0], enginePins[1]);  
+  braked(enginePins[4], enginePins[5]);  
+}  
+  
+// Rotate the robot to the left  
+void rotateTheRobotLeft() {  
+  engineMovementClockwise(enginePins[0], enginePins[1]);  
+  engineMovementClockwise(enginePins[4], enginePins[5]);  
+  braked(enginePins[2], enginePins[3]);  
+  braked(enginePins[6], enginePins[7]);  
+}  
+  
+// Delay and then stop the robot  
+void runtime(int timeDelay) {  
+  delay(timeDelay);  
+  stopTheRobot();  
+}  
+  
+void setup() {  
+  Serial.begin(9600);  
+  setPinModeOut();  
+}  
+  
+void loop() {  
+  int timeDelay = 2000;  
+  if (Serial.available()) {  
+    char commandRead = Serial.read();  
+    switch (commandRead) {  
+      case FORWARD:  
+        moveTheRobotForward();  
+        runtime(timeDelay);  
+        break;  
+      case BACK:  
+        moveTheRobotBack();  
+        runtime(timeDelay);  
+        break;  
+      case LEFT:  
+        rotateTheRobotLeft();  
+        runtime(timeDelay);  
+        break;  
+      case RIGHT:  
+        rotateTheRobotRight();  
+        runtime(timeDelay);  
+        break;  
+      case STOP:  
+        stopTheRobot();  
+        break;  
+    }  
+  }  
+}  
